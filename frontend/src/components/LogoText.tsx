31c8ab880555dd7379e08d0b663da9d0c40538e4
aukri1690 <toro369@icloud.com>
2026-05-30 16:01:01 +0900

    feat: implement notes and change font size

--- frontend/src/components/LogoText.tsx
+++ frontend/src/components/LogoText.tsx
@@ -3,7 +3,7 @@
 const LogoText = () => {
   return ( 
     <div className="text-center ">
-    <h6 className="text-xs text-muted-foreground">毎日の睡眠を可視化する</h6>
+    <h6 className="text-sm text-muted-foreground">毎日の睡眠を可視化する</h6>
     <h1 className="text-smarthr text-4xl font-semibold tracking-tight">Sleep Log</h1>
     </div>
   )
